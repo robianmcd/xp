341ffc714eac94105bacda515a9f309aea63bc63
Rob <robianmcd@gmail.com>
2014-09-29 20:23:07 -0400

    Added scroll to experiences

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var app = angular.module('xp', ['ui.bootstrap']);
 
 app.constant('config', window.config);
 
-function AppCtrl(config) {
+
+
+function AppCtrl(config, anchorSmoothScroll) {
     this.config = config;
+    this.anchorSmoothScroll = anchorSmoothScroll;
 }
 
 app.controller('AppCtrl', AppCtrl);
+
+AppCtrl.prototype.scrollToId = function(id) {
+    this.anchorSmoothScroll.scrollTo(id);
+};
---
+++ services/anchorSmoothScrollService.js
@@ -0,0 +1,67 @@
+(function() {
+    var app = angular.module('xp');
+
+    var AnchorSmoothScroll = function() {
+
+    };
+
+    AnchorSmoothScroll.$inject = [];
+    app.service('anchorSmoothScroll', AnchorSmoothScroll);
+
+
+    AnchorSmoothScroll.prototype.scrollTo = function(eID) {
+
+        // This scrolling function
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 80);
+
+
+        if (speed <= 8) speed = 8;
+        if (speed >= 18) speed = 18;
+
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var j=startY; j>stopY; j-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+
+})();
+
